2c61a0e47fc905c7574231f11b6a5b8f184213b3
Will Wedmedyk <willwedmedyk@gmail.com>
2023-12-26 10:46:36 -0500

    ww/image parsing: add image parsing using goseract

--- handlers/go.mod
+++ handlers/go.mod
@@ -2,12 +2,18 @@ module api/internal/handlers
 
 go 1.21.3
 
-require github.com/go-rod/rod v0.114.5
+require (
+	api/internal/database v0.0.0-00010101000000-000000000000
+	github.com/go-rod/rod v0.114.5
+)
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/ysmood/fetchup v0.2.3 // indirect
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/got v0.34.1 // indirect
 	github.com/ysmood/gson v0.7.3 // indirect
 	github.com/ysmood/leakless v0.8.0 // indirect
 )
+
+replace api/internal/database => ../database
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,16 +1,99 @@
 package handlers
 
 import (
+	"api/internal/database"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/go-rod/rod"
+	"github.com/otiai10/gosseract/v2"
 )
+
+const ErrorImage = "must use jpeg, png, or tiff file types"
 
 func Healthcheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Healthcheck!"})
+}
+
+func ImageHandler(w http.ResponseWriter, r *http.Request) {
+	file, hdr, err := r.FormFile("image")
+	if err != nil {
+		// If there is an error that means form is empty. Return nil for err in order
+		// to validate result as required.
+		fmt.Println("error at reading form")
+	}
+	defer file.Close()
+
+	// Create physical file
+	tempfile, err := os.CreateTemp("", "ocrserver"+"-")
+	if err != nil {
+		fmt.Println("error at temp")
+	}
+	defer func() {
+		tempfile.Close()
+		os.Remove(tempfile.Name())
+	}()
+
+	switch hdr.Header.Get("Content-Type") {
+	case "application/octet-stream":
+		// add conversion later for fun?
+		json.NewEncoder(w).Encode(map[string]string{"message": ErrorImage})
+		break
+	case "image/tiff":
+		fallthrough
+	case "image/png":
+		fallthrough
+	case "image/jpeg":
+		// Make uploaded physical
+		if _, err = io.Copy(tempfile, file); err != nil {
+			fmt.Println("error at copy")
+		}
+
+		client := gosseract.NewClient()
+		defer client.Close()
+
+		client.SetImage(tempfile.Name())
+		// client.SetLanguage("deu") // need to receive lang from api keys
+		client.Languages = []string{"eng"}
+		if langs := r.FormValue("languages"); langs != "" {
+			client.Languages = strings.Split(langs, ",")
+		}
+
+		pageText, err := client.Text()
+		if err != nil {
+			fmt.Println("error at text out: ", err)
+		}
+		json.NewEncoder(w).Encode(map[string]string{"message": pageText})
+
+	default:
+		json.NewEncoder(w).Encode(map[string]string{"message": ErrorImage})
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("test login")
+	rows, err := database.DB().Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	type Users struct {
+		name         string
+		first        string
+		last         string
+		time_created string
+	}
+	fmt.Printf("%+v\n", rows)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "login handler fired!"})
 }
 
 func scrape(w http.ResponseWriter, r *http.Request) {
