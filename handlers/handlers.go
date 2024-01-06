66f74203e75afd592877389ab4aa1fa3225e1b40
Will Wedmedyk <willwedmedyk@gmail.com>
2024-01-06 09:58:52 -0500

    ww/refactor: clean scraper and write basic body scrape for now

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -99,62 +99,18 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "login handler fired!"})
 }
 
-func scrape(w http.ResponseWriter, r *http.Request) {
+func ScrapeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	// https://www.spiegel.de/international/world/escalating-violence-radical-settlers-on-the-west-bank-see-an-opportunity-a-9499f824-9b39-4739-b6db-36772bc2bb99
 	// Decode the request body into the User struct
 	url := r.URL.Query().Get("url")
-	fmt.Println("url =>", url)
-
+	fmt.Println("scraping url =>", url)
 	// Launch a new browser with default options, and connect to it.
-
 	browser := rod.New().MustConnect()
-
-	// close it after main process ends.
 	defer browser.MustClose()
-
 	// Create a new page
 	page := browser.MustPage(url).MustWaitStable()
-	//DER SPIEGEL EXAMPLE
-	//heading
-	// fmt.Println(page.MustElement("#Inhalt > article > header > div > div").MustEval(`() => this.innerText`).String())
-	heading := page.MustElement("#Inhalt > article > header > div > div").MustEval(`() => this.innerText`).String()
-	// fmt.Printf(heading)
-
-	// main := page.MustElement("main").MustEval(`() => {
-	// 	return this.innerText
-	// 	}`)
-	// fmt.Printf("%+v\n", main)
-
-	main := page.MustElements("header")
-	fmt.Printf("%+v\n", main)
-
-	// main := page.MustSearch("main").MustEval(`() => {
-	// 	return this
-	// 	}`)
-	// fmt.Printf("____  TEST  ____ \n%+v\n %T", main, main)
-	// 	var res
-	// 	err := json.Unmarshal([]byte(str), &res)
-	// 	fmt.Println(err)
-	// 	fmt.Println(res)
-	// 	for _, m := range main {
-
-	//     // m is a map[string]interface.
-	//     // loop over keys and values in the map.
-	//     for k, v := range m {
-	//         fmt.Println(k, "value is", v)
-	//     }
-	// }
-
-	// header := page.MustElement("heading")
-	// fmt.Printf("%+v\n", header)
-
-	text := page.MustElements("p")
-	fmt.Printf("%+v\n %T", text, text)
-	//body
-	// fmt.Println(page.MustElement("#Inhalt > article > div.relative > section.relative > div > div").MustEval(`() => this.innerText`).String())
-	body := page.MustElement("#Inhalt > article > div.relative > section.relative > div > div").MustEval(`() => this.innerText`).String()
-	// fmt.Printf(body)
-
+	body := page.MustElement("main").MustEval(`() => this.innerText`).String()
+	heading := page.MustElement("h1").MustEval(`() => this.innerText`).String()
 	json.NewEncoder(w).Encode(map[string]string{"heading": heading, "body": body})
 }
