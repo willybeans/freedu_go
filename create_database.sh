4e8ad92f442acf8b5748cfb5bfb10b259ece2d43
Will Wedmedyk <willwedmedyk@gmail.com>
2024-01-26 13:23:46 -0500

    ww/refactor: rewriting bash script and DB tables, using CSV file for mock data

--- create_database.sh
+++ create_database.sh
@@ -7,38 +7,49 @@ if ! command -v psql &> /dev/null; then
 fi
 
 # PostgreSQL database credentials
-DB_HOST="localhost"
-DB_PORT="5432"
-DB_NAME="lang_api"
-DB_USER="postgres"
-DB_PASSWORD="password"
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+    source .env
+fi
+
+# DB_HOST="localhost"
+# DB_PORT="5432"
+# DB_NAME="lang_api"
+# DB_USER="postgres"
+# DB_PASSWORD="password"
 
 # SQL statements to create the database and table
 SQL_CREATE_DB="CREATE DATABASE $DB_NAME;"
-SQL_CREATE_TABLE="CREATE TABLE users (
-    id INT,
-    age INT,
-    username TEXT,
-    password TEXT,
-    time_created INT,
-    first_name TEXT,
-    last_name TEXT,
-    email TEXT
+SQL_CREATE_TABLE_USERS="CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    time_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );"
-SQL_CREATE_SEQUENCE="CREATE SEQUENCE id_seq
-  START WITH 1
-  INCREMENT BY 1
-  MINVALUE 1
-  NO MAXVALUE
-  CACHE 1;
+SQL_CREATE_TABLE_CONTENT="CREATE TABLE content (
+    content_id SERIAL PRIMARY KEY,
+    author_id INTEGER REFERENCES users(user_id),
+    title VARCHAR(255) NOT NULL,
+    body_content TEXT,
+    time_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);"
+
+SQL_INSERT_DATA="INSERT INTO users (username) VALUES
+  ('johnny_mnemonic')
 "
 
 # Create the database
 psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -c "$SQL_CREATE_DB"
 
-# Create the table
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$SQL_CREATE_TABLE"
+# Create the tables
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$SQL_CREATE_TABLE_USERS"
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$SQL_CREATE_TABLE_CONTENT"
 
-# Create Sequence
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$SQL_CREATE_SEQUENCE"
+# Create default user
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$SQL_INSERT_DATA"
 
+# Create mock data with csv file
+CSV_FILE="MOCK_DATA.csv"
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "\COPY content FROM '$CSV_FILE' delimiter ',' CSV HEADER;"
+# in psql you can also run this command: 
+#  \copy content from '/Users/willwedmedyk/Downloads/MOCK_DATA.csv' delimiter ',' CSV HEADER;
