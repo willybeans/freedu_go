8fd6ff27eca12918cf2368efb399885d9fd214a8
Will Wedmedyk <willwedmedyk@gmail.com>
2024-02-22 09:19:50 -0500

    ww/chats: update types

--- types/chat.go
+++ types/chat.go
@@ -24,3 +24,16 @@ type IdsForNewXref struct {
 	User_ID     string `json:"user_id"`
 	ChatRoom_ID string `json:"chat_room_id"`
 }
+
+// type ChatPreview struct {
+// 	Members     []string       `json:"members"`
+// 	ChatRoom_ID string         `json:"chat_room_id"`
+// 	Preview     PreviewMessage `json:preview`
+// }
+
+type PreviewMessage struct {
+	Username  string   `json:"username"`
+	Content   string   `json:"content"`
+	SentAt    string   `json:"sent_at"`
+	UserNames []string `json:"usernames"`
+}
